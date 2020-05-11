538183703ae2bbfb7fbd11b000e55e07df9a614e
Ani2817 <anirudh.pcrafter@gmail.com>
2020-05-11 19:31:39 +0530

    table in database
    updated

--- wp-admin/upgrade-functions.php
+++ wp-admin/upgrade-functions.php
@@ -10,3 +10,29 @@
 
 _deprecated_file( basename( __FILE__ ), '2.5.0', 'wp-admin/includes/upgrade.php' );
 require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+
+
+
+function create_plugin_database_table()
+{
+    global $table_prefix, $wpdb;
+
+    $tblname = 'pin';
+    $wp_track_table = $table_prefix . "$tblname ";
+
+    #Check to see if the table exists already, if not, then create it
+
+    if($wpdb->get_var( "show tables like '$wp_track_table'" ) != $wp_track_table) 
+    {
+
+        $sql = "CREATE TABLE `". $wp_track_table . "` ( ";
+        $sql .= "  `id`  int(11)   NOT NULL auto_increment, ";
+        $sql .= "  `pincode`  int(128)   NOT NULL, ";
+        $sql .= "  PRIMARY KEY `order_id` (`id`) "; 
+        $sql .= ") ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ; ";
+        require_once( ABSPATH . '/wp-admin/includes/upgrade.php' );
+        dbDelta($sql);
+    }
+}
+
+ register_activation_hook( __FILE__, 'create_plugin_database_table' );
--- wp-content/plugins/form-submission/reload.php
+++ wp-content/plugins/form-submission/reload.php
@@ -1,24 +1,49 @@
 <?php
+  function create_plugin_database_table()
+{
+    global $wpdb;
+    require_once(ABSPATH.'wp-admin/includes/upgrade.php');
+
+    if(count($wpdb->get_var( 'show tables like "$wp_custom_table"' ))==0) 
+    {
+
+        $sql = "CREATE TABLE  wp_anicustom_table( 
+          firstname varchar(11) NOT NULL ,
+          lastname  varchar(128)   NOT NULL
+          ) ENGINE=MyISAM DEFAULT CHARSET=latin1" ;
+
+       
+  
+        dbDelta($sql);
+    }
+}
+ register_activation_hook(__FILE__,'create_plugin_database_table');
+
+
+?>
+
+
+<?php
 if(isset($_POST['SubmitButton'])){ //check if form was submitted
-  $conn = new mysqli('localhost','root','','test1');
+  $conn = new mysqli('localhost','root','','new-wordpress');
   
   $firstname=$_POST['firstname'];
   $lastname=$_POST['lastname'];
 
-  if ($conn->query("INSERT INTO test1 VALUES('$firstname','$lastname')") === TRUE) {
+  if ($conn->query("INSERT INTO wp_anicustom_table VALUES('$firstname','$lastname')") === TRUE) {
   echo "successfully inserted";
-	} 
-	else {
-	  echo "insertion failed" ;
-	}
+  } 
+  else {
+    echo "insertion failed" ;
+  }
 
-	$conn->close();
+  $conn->close();
 }    
 ?>
 
 <?php      
 /*
-plugin name:reload plugin
+plugin name:submission plugin
 description:this is a simple plugin for purpose of learning
 version:1.0.0
 author:Anirudh pandey
@@ -43,4 +68,5 @@ function input_fields( $atts ) {
    <?php
 }
 ?>
+
 <script src="my_script.js" type="text/javascript"></script>
